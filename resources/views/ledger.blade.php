0745b47dfb0adc80df816afab6600c1f98eb7773
Филозоп Алексей <a.filozop@wisetech.pro>
2023-05-03 05:53:00 +0300

    [lab2] зменить шаблон страницы отзывов

--- resources/views/ledger.blade.php
+++ resources/views/ledger.blade.php
@@ -12,19 +12,29 @@
             <h2>Отзывы</h2>
         </div>
         <div class="card-content">
+            @if (count($comments) > 0)
+                @foreach ($comments as $comment)
+                    <x-comment
+                        :author-firstname="$comment->firstname"
+                        :author-lastname="$comment->lastname"
+                        :email="$comment->email"
+                        :comment-body="$comment->body_text"
+                        :created-at="$comment->created_at" />
+                @endforeach
+                {{ $comments->links('pagination.default') }}
+            @else
+                <p>Nobody here but us chickens!</p>
+            @endif
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header"><h2>Добавить отзыв</h2></div>
+        <div class="card-content">
             <form action="/ledger" enctype="multipart/form-data" method="POST">
                 <input type="file" name="uploaded_file" id="ledger-file-input" />
                 <input type="submit" value="Отправить" />
             </form>
-            @foreach ($comments as $comment)
-                <x-comment
-                    :author-firstname="$comment->firstname"
-                    :author-lastname="$comment->lastname"
-                    :email="$comment->email"
-                    :comment-body="$comment->body_text"
-                    :created-at="$comment->created_at" />
-            @endforeach
-            {{ $comments->links('pagination.default') }}
         </div>
     </div>
 @endsection
